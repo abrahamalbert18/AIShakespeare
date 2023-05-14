06f1617796fde9d277d05e12c44103f3c29b726d
Abraham Albert <abrahamalbert18@icloud.com>
2023-05-15 04:04:12 +1000

    Loads the dataset using PyTorch Dataset module.

--- Dataset.py
+++ Dataset.py
@@ -1,16 +1,34 @@
-def loadData(numberOfLines):
-    with open(f"ShakespeareBooks/CompleteWorksOfShakespeare.txt", "r") as file:
-        data = file.readlines()
-        data = removeBlankLines(data)
-        print(data[:numberOfLines])
+import torch
+from torch.utils.data import Dataset
 
-def removeBlankLines(data):
-    cleanedData = []
-    for line in data:
-        line = line.strip()
-        if len(line) > 1:
-            cleanedData.append(line)
-    return cleanedData
+class ShakespeareDataset(Dataset):
+    def __init__(self, filename = f"ShakespeareBooks/CompleteWorksOfShakespeare.txt"):
+        super().__init__()
+        self.filename = filename
+        self.data = self.loadData()
+
+    def removeBlankLines(self, data):
+        cleanedData = []
+        for line in data:
+            line = line.strip()
+            if len(line) > 1:
+                cleanedData.append(line)
+        return cleanedData
+    def loadData(self):
+        with open(self.filename, "r") as file:
+            data = file.readlines()
+            data = self.removeBlankLines(data)
+        return data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, item):
+        return self.data[item]
 
 if __name__=="__main__":
-    loadData(numberOfLines=100)
+    text = ShakespeareDataset()
+    for i in range(10):
+        print(text[i])
+
+    print(f"Total length of the dataset = {len(text)}")
