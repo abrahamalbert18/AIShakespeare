bc592b7aa0186798ea1d4b662f48ec0586b4ecf1
Abraham Albert <abrahamalbert18@icloud.com>
2023-05-28 00:50:04 +1000

    Shakespeare's brain architecture is configured. It works well with the custom tokenizer. Passed multiple test cases.

--- Models.py
+++ Models.py
@@ -1,16 +1,35 @@
 import torch
 from torch import nn
+from Dataset import ShakespeareDataset
 
 class ShakespeareBrain(nn.Module):
-    def __init__(self, maxInputSequenceLength, numOfOutputWords):
+    def __init__(self):
         super().__init__()
-        self.numOfOutputs = numOfOutputWords
-        self.inputSequenceLength = maxInputSequenceLength
-        self.transformerNetwork = nn.Transformer(nhead=8, batch_first=True)
+        self.vocabSize = 30000
+        self.inputSequenceLength = 32
+        self.embedding = nn.Embedding(self.vocabSize, self.inputSequenceLength)
+        self.transformerNetwork = nn.Transformer(nhead=8, batch_first=True,
+                                                 d_model=32)
 
-    def forward(self, encoderInputs, decoderInputs):
-        outputs = self.transformerNetwork(encoderInputs, decoderInputs)
+    def forward(self, encoderInputs, decoderInputs, sourceMask):
+        source = self.embedding(encoderInputs)
+        target = self.embedding(decoderInputs)
+        outputs = self.transformerNetwork(src=source, tgt=target,
+                                          src_mask=sourceMask)
         return outputs
 
 if __name__=="__main__":
-    model = ShakespeareBrain(20,20)
+    model = ShakespeareBrain()
+
+    def simpleTest(index=1):
+        text = ShakespeareDataset()
+        inputs = text[index]
+        source = inputs["sourceIds"]
+        target = inputs["targetIds"]
+        sourceMasks = inputs["sourceMasks"].bool()
+        outputs = model(source, target, sourceMasks)
+        print(outputs.shape)
+        return outputs
+    # outputs = simpleTest()
+    # for i in range(800,1800):
+    #     simpleTest(i)
