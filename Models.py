4d3795b86986194ab3f185715988b81668d5b1c2
Abraham Albert <abrahamalbert18@icloud.com>
2023-06-14 17:16:49 +1000

    Now model can be trained either using regression mode or classification mode.

--- Models.py
+++ Models.py
@@ -3,17 +3,25 @@ from torch import nn
 from Dataset import ShakespeareDataset
 
 class ShakespeareBrain(nn.Module):
-    def __init__(self, contextLength=32):
+    def __init__(self, numberOfHeads=4, contextLength=32, classification=True):
         super().__init__()
         self.vocabSize = 30000
         self.contextLength = contextLength
+        self.numberOfHeads = numberOfHeads
+        self.classifcation = classification
         # self.oneHotEncoding = torch.zeros(self.vocabSize, self.vocabSize)
         self.wordEmbedding = nn.Embedding(self.vocabSize, self.contextLength)
         self.positionEmbedding = nn.Embedding(self.vocabSize,
                                               self.contextLength)
-        self.transformerNetwork = nn.Transformer(nhead=8, batch_first=True,
+        self.transformerNetwork = nn.Transformer(nhead=self.numberOfHeads,
+                                                 batch_first=True,
                                                  d_model=self.contextLength)
-        self.criterion = nn.CrossEntropyLoss()
+        if self.classifcation:
+            self.criterion = nn.CrossEntropyLoss() #classification
+        else:
+            self.criterion = nn.MSELoss(reduction="sum")
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
         self.layerNorm = nn.LayerNorm(self.contextLength)
 
     def forward(self, encoderInputs, decoderInputs, sourceMask):
@@ -27,9 +35,20 @@ class ShakespeareBrain(nn.Module):
                  self.positionEmbedding(position)
         outputs = self.transformerNetwork(src=source, tgt=target,
                                           src_mask=sourceMask)
-        predictionLayer = nn.Linear(T * self.contextLength,
-                            self.vocabSize).to(encoderInputs.device)
-        outputs = predictionLayer(outputs.view(B, -1))
+        if not self.classifcation:
+            # Regression
+            predictionLayer = nn.Linear(T * self.contextLength, 1).to(
+                                        encoderInputs.device)
+
+            outputs = predictionLayer(outputs.view(B, -1)).squeeze()
+            # outputs = self.relu(outputs)
+            targetTokens = torch.div(targetTokens, self.vocabSize)
+        else:
+        # Classification
+            predictionLayer = nn.Linear(T * self.contextLength,
+                                self.vocabSize).to(encoderInputs.device)
+            outputs = predictionLayer(outputs.view(B, -1))
+
         loss = self.criterion(outputs, targetTokens)
         return outputs, loss
 
