ff40674597e5bed3517d7f4c47801d2ecacb8325
Abraham Albert <abrahamalbert18@icloud.com>
2024-01-03 16:19:38 +1100

    More efficient code is written for text generation.

--- GenerateText.py
+++ GenerateText.py
@@ -8,7 +8,7 @@ parser.add_argument("-n", "--modelName",
                     default=f"ShakespeareWith-->8Heads+CL-->512+VocabSize-->2000.pth.tar")
 parser.add_argument("-nv", "--cuda", default=False, type=bool)
 parser.add_argument("-t", "--tokens", default=500, type=int)
-parser.add_argument("-w", "--word", default="love", type=str)
+parser.add_argument("-w", "--word", default="she", type=str)
 parser.add_argument("-v", "--vocabSize", default=2000, type=int)
 parser.add_argument("-cl", "--contextLength", default=256, type=int)
 
@@ -24,8 +24,8 @@ contextLength = args.contextLength
 tokenizer = Tokenizer.from_file(path="Tokenizer/Vocab.json")
 sentence = f"{firstWord} "
 tokenizedSentence = tokenizer.encode(sequence=sentence)
-tokenizedTarget = tokenizedSentence.ids[1:] + [2]
-source = torch.tensor(tokenizedSentence.ids)
+tokenizedTarget = tokenizedSentence.ids[1:]
+source = torch.tensor(tokenizedSentence.ids[:-1])
 target = torch.tensor(tokenizedTarget)
 
 if not cuda:
@@ -44,25 +44,26 @@ model.eval()
 
 predictedTokens = torch.zeros(numberOfTokens)
 predictedTokensPerLine = torch.zeros(25)
+maxLength = 25
+predictedWords = []
 print(f"{'-'*40}\n\n")
-for l in range(numberOfTokens//25):
-    for i in range((25 - source.size(-1))//2):
-        outputs = model(source.unsqueeze(0), target.unsqueeze(0)) # Encoder-Decoder
-        nextTokenProbs = outputs[-1].softmax(dim=-1)
-        predictions = torch.multinomial(nextTokenProbs,
-                                        num_samples=1).to("cpu")
+for l in range(numberOfTokens):
+    if source.size(-1) >= maxLength:
+        words = tokenizer.decode(target[:-1].short().tolist())
+        # predictedWords.append(words)
+        print(words)
+        source = source[-1:]
+        target = target[-1:]
 
-        predictedTokensPerLine[:source.size(-1)] = source.clone()
-        predictedTokensPerLine[source.size(-1) + i] = predictions.item()
-        source = predictedTokensPerLine[:source.size(-1) + 1].clone()
-        target = predictedTokensPerLine[1:source.size(-1) + 1].clone()
-        target[-1] = 2
+    outputs = model(source.unsqueeze(0), target.unsqueeze(0)) # Encoder-Decoder
+    nextTokenProbs = outputs[-1].softmax(dim=-1)
+    predictions = torch.multinomial(nextTokenProbs,
+                                    num_samples=1).to("cpu")
+    source = torch.cat((source, predictions))
+    target = torch.cat((target, predictions))
+    # print(tokenizer.decode(predictions.tolist()))
 
-        # print(f"Prediction = {predictions}")
-        # print(f"Source, Target: {source}, {target}")
-    predictedTokens[l * 25: (l + 1) * 25] = predictedTokensPerLine
-    predictedWords = tokenizer.decode(predictedTokensPerLine.short().tolist())
-    print(predictedWords)
-    source, target = source[-2:-1], target[-1:]
+# predictedWords = tokenizer.decode(target.short().tolist())
+# print("\n".join(predictedWords))
 
 print(f"{'-'*40}")
