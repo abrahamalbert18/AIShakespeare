acce9405ce06fcddcee6c3cc3bc735cbade298dd
Abraham Albert <abrahamalbert18@icloud.com>
2024-01-09 11:58:38 +1100

    Added a new class for Positional Encoding based on Attention is All You Need paper.

--- Models.py
+++ Models.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from Dataset import ShakespeareDataset
+import math
 
 class ShakespeareBrain(nn.Module):
     def __init__(self, numberOfHeads=4, contextLength=32, classification=True,
@@ -68,7 +69,39 @@ class ShakespeareBrain(nn.Module):
         loss = self.criterion(outputs, decoderInputs.long().view(-1))
         return outputs, loss
 
+#TODO
+class PositionalEncoding(nn.Module):
+    def __init__(self, contextLength, maxSequenceLength=30,
+                 dropout=0.1):
+        super().__init__()
+        # self.dropout = nn.Dropout(p=dropout)
+
+        positionalEmbedding = torch.zeros(maxSequenceLength, contextLength)
+        #PositionalEmbedding
+        position = torch.arange(maxSequenceLength).unsqueeze(1)
+        # According to "Attention is All You Need" Paper
+        scaleFactor = torch.exp(torch.arange(0, contextLength, 2)
+                                * (-math.log(10000.0) / contextLength))
+        positionalEmbedding[:, 0::2] = torch.sin(position * scaleFactor)
+        positionalEmbedding[:, 1::2] = torch.cos(position * scaleFactor)
+        positionalEmbedding = positionalEmbedding.unsqueeze(0)
+        self.register_buffer("positionalEmbedding", positionalEmbedding)
+
+    def forward(self, inputs):
+        # @torch.no_grad
+        return inputs + self.positionalEmbedding[:, :inputs.shape[1],
+                        :].requires_grad_(False)
+        # return self.dropout(x)
+
+
 if __name__=="__main__":
+
+    p = PositionalEncoding(contextLength=10,maxSequenceLength=5)
+    inputs = torch.rand((2, 4, 10))
+    pEmbedded = p(inputs)
+    print(pEmbedded)
+
+
     model = ShakespeareBrain()
 
     def simpleTest(index=1):
